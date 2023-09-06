ea2615ff59160913abdfbef0db3f134eed509927
Justin Lee <justinlee@Justins-Air.nyc.rr.com>
2023-09-06 14:14:26 -0400

    As the types package does not publish actual JavaScript code, we only need to run the TS compiler for build

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -1,5 +1,4 @@
 import reactConfig from './react/rollup.config.js';
 import utilsConfig from './utils/rollup.config.js';
-import typesConfig from './types/rollup.config.js';
 
-export default [reactConfig, typesConfig, utilsConfig];
+export default [reactConfig, utilsConfig];
